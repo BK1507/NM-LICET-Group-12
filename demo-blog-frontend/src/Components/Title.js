f4bbfbc1ca4e2dfa4e52c42683ca5b0ad3c217d1
keerthanaa <keerthanaa.24it@licet.ac.in>
2023-11-25 19:20:35 +0530

    commited successfully frontend and backend

---
+++ demo-blog-frontend/src/Components/Title.js
@@ -0,0 +1,32 @@
+import React from "react";
+import ReactLogo from "./ReactLogo";
+import SpringLogo from "./SpringLogo";
+import RestApiLogo from "./RestApiLogo";
+
+const Title = () => {
+  return (
+    <div className="btn" style={{ cursor: "default" }}>
+      <h1>
+        
+        <SpringLogo />
+        <span
+          className="me-2"
+          style={{
+            color: "#61dafb",
+            textShadow: "2px 2px 3px rgb(0,88,121)",
+          }}
+        >
+          The BlogSpot 
+        </span>
+        
+        
+       
+       
+        
+       
+      </h1>
+    </div>
+  );
+};
+
+export default Title;
